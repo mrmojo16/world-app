7c0d11907f06b5c8f3938d43b7c782b41ad2596c
Luis Felipe Mahecha <mahechagiraldoluisfelipe@gmail.com>
2026-03-10 16:12:00 -0500

    Controlador y lógica de consulta de ciudades

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-Hello world
+@"
+<?php
+require_once 'app/controllers/CityController.php';
+
+\$controller = new CityController();
+
+\$action = isset(\$_GET['action']) ? \$_GET['action'] : 'index';
+
+switch(\$action) {
+    case 'getCities':
+        \$controller->getCities();
+        break;
+    case 'searchCountry':
+        \$controller->searchCountry();
+        break;
+    case 'topCities':
+        \$controller->topCities();
+        break;
+    default:
+        \$controller->index();
+        break;
+}
+?>
+"@ | Out-File -FilePath index.php -Encoding UTF8
